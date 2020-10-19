fe0a51abf8d0ed1a80f2f3ab20a87ffaee9fb27d
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2020-10-19 18:00:25 -0300

    Iniciando projeto to-do com Vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container grid-xs py-2">
+      <img
+        class="img-responsive img-logo"
+        src="@/assets/nezuko.png"
+        alt="qualquer imagem"
+      />
+
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input
+            type="text"
+            v-model="todo.description"
+            class="form-input"
+            placeholder="Novo todo"
+          />
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+
+      <div class="todo-list">
+        <todo v-for="t in todos" :key="t.id" :todo="t"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from "./components/Todo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    Todo,
+  },
+
+  data() {
+    return {
+      todos: [],
+      todo: {
+        checked: false,
+      },
+    };
+  },
+
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+      this.todo = { checked: false };
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.img-logo {
+  max-width: 200px;
+  margin: 0 auto;
+}
+
+.todo-list {
+  padding-top: 2rem;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tile flex-centered">
+    <div class="tile-icon">
+      <div>
+        <i class="icon icon-time centered"></i>
+      </div>
+    </div>
+
+    <dir class="tile-content">
+      <div class="tile-subtitle">{{ todo.description }}</div>
+    </dir>
+
+    <div class="tile-action">
+      <button class="btn btn-link">Concluído</button>
+      <button class="btn btn-link">
+        <span class="text-error">Remover</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+
+<style>
+</style>
